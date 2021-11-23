ce7ab3c136dc04d590259a4b9b1b6af58fc356ae
Joyce.Yeung <joyce.yeung@myob.com>
2021-11-24 09:14:19 +1100

    added Card class to start with

---
+++ Kata-BlackJack.Test/CardTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace Kata_BlackJack.Test
+{
+    public class CardTest
+    {
+        [Fact]
+        public void AnAceOfHeart_ShouldHaveASuitOfHeart()
+        {
+            var suit = "Heart";
+            var card = new Card(suit);
+
+            Assert.Equal(card.Suit, "Heart");
+        }
+
+    }
+}
---
+++ Kata-BlackJack/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Kata_BlackJack
+{
+    public class Card
+    {
+        public String Suit { get; set; }
+
+        public Card(String suit)
+        {
+            Suit = suit;''
+        }
+    }
+}
